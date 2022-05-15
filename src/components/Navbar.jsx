a08c7950820d5744c87735e0fca875c88b45b90a
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-15 20:31:01 +0300

    Adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <Nav>
+        <div className="brand">
+          <div className="container">
+            <img src={logo} alt="" />
+            Kamıon
+          </div>
+          <div className="toggle"></div>
+        </div>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+        <div>
+          {" "}
+          <button>Log In</button>
+          <button>Sıgn Up</button>
+        </div>
+      </Nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .brand {
+    .container {
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.4rem;
+      font-size: 1.2rem;
+      font-weight: 900;
+      text-transform: uppercase;
+    }
+    .toggle {
+      display: none;
+    }
+  }
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 1rem;
+    li {
+      a {
+        text-decoration: none;
+        color: #0077b6;
+        font-size: 1.2rem;
+        transition: 0.1ms ease-in-out;
+        &:hover {
+          color: #023e8a;
+        }
+      }
+      &:first-of-type {
+        a {
+          color: #023e8a;
+          font-weight: 900;
+        }
+      }
+    }
+  }
+  button {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border-radius: 1rem;
+    border: none;
+    color: white;
+    background-color: #48cae4;
+    text-transform: uppercase;
+    font-size: 1.1rem;
+    letter-spacing: 0.1rem;
+    transition: 0.3s ease-in-out;
+    &:hover {
+      background-color: #023e8a;
+    }
+  }
+`;
