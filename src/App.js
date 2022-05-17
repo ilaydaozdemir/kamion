d0bf624ecddfba9e100f09696982b780549122ad
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-17 22:23:25 +0300

    Add routes for Home, About, Login and SignUp pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Navbar from './components/Navbar';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import About from './pages/About';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
-  return <>
-    <Navbar />
-  </>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="sign-up" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
