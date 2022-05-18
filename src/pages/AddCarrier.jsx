bb5f6975f450c2db515bcf0030e1cad9f8f061cf
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-18 20:38:26 +0300

    Add  AddCarrier page

--- src/pages/AddCarrier.jsx
+++ src/pages/AddCarrier.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 const AddCarrier = () => {
   return (
-    <div>
-      <h1>Add Carrier</h1>
-    </div>
+    <AddCarriers>
+      <h1> Add Carrier</h1>
+    </AddCarriers>
   );
 };
 
 export default AddCarrier;
+const AddCarriers = styled.div`
+  margin-top: 2rem;
+  width: 100%;
+  height: 100%;
+  h1 {
+    color: #023e8a;
+    font-size: 2rem;
+    letter-spacing: 0.1rem;
+  }
+`;
